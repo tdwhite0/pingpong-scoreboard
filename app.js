8d97a5a05ee05688cb1acce84f5e2234dff935a3
tdwhite0 <tdwhite0@gmail.com>
2015-12-29 21:04:51 -0500

    cleanup and send statuses

--- app.js
+++ app.js
@@ -10,37 +10,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
+var socketio = io(http).listen(server);
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/server/index.html');
 });
 app.get('/one', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('one', { for: 'everyone' });
 });
 app.get('/one-down', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('one-down', { for: 'everyone' });
 });
 app.get('/two', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('two', { for: 'everyone' });
 });
 app.get('/two-down', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('two-down', { for: 'everyone' });
 });
-var server = http.createServer(app);
-var socketio = io(http).listen(server);
-socketio.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        socketio.emit('chat message', msg);
-    });
-});
-//var server = app.listen(port, function () {
-//    var host = server.address().address;
-//    var port = server.address().port;
-//    console.log('This express app is listening on %s', host, port);
-//});
 server.listen(port);
 console.log('This express app is listening on %s', port);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -9,9 +9,6 @@ import bodyParser = require('body-parser');
 
 let port = process.env.port || 3000;
 
-
-
-
 let app = express();
 
 app.use(bodyParser.json());
@@ -19,53 +16,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+let server = http.createServer(app);
+let socketio = io(http).listen(server);
 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/server/index.html');
 });
 
-
-
-
-app.get('/one', function (req, res) {
-    res.send('<div>cool</div>');
+app.get('/one', function (req: express.Request, res: express.Response) {
+    res.sendStatus(200);
     socketio.emit('one', { for: 'everyone' });
 });
 
 app.get('/one-down', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('one-down', { for: 'everyone' });
 });
 
 app.get('/two', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('two', { for: 'everyone' });
 });
 
 app.get('/two-down', function (req, res) {
-    res.send('<div>cool</div>');
+    res.sendStatus(200);
     socketio.emit('two-down', { for: 'everyone' });
 });
-
-let server = http.createServer(app);
-
-let socketio = io(http).listen(server);
-
-socketio.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        socketio.emit('chat message', msg);
-    });
-});
-
-//var server = app.listen(port, function () {
-//    var host = server.address().address;
-//    var port = server.address().port;
-//    console.log('This express app is listening on %s', host, port);
-//});
-
-
 
 server.listen(port);
 console.log('This express app is listening on %s', port);
