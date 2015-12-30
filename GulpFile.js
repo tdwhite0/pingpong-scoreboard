8cbb38676697fe976cb7dc244b247df8d8154fbc
Rich Grenwick <rgrenwick@gmail.com>
2015-12-30 14:06:43 -0500

    got it working with vs2015 added default task to gulpfile

--- GulpFile.js
+++ GulpFile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-
+var typescript = require('gulp-tsc');
 
 gulp.task('sass', function () {
   gulp.src('./client/**/*.scss')
@@ -15,3 +15,12 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+
+gulp.task('tsc:compile', function () {
+    gulp.src(['src/**/*.ts'])
+    .pipe(typescript())
+    .pipe(gulp.dest('public/'))
+});
+
+gulp.task('default',['sass','tsc:compile'])
--- client/App.js
+++ client/App.js
@@ -1,3 +1,4 @@
+/// <reference path="../typings/tsd.d.ts" />
 var angular = require("angular");
 require("./scoreboard/scoreboard");
 require("./App.scss");
